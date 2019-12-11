5b6afd7db154083bd573f578dd59dc233a9536ab
WGF <546405494@qq.com>
2019-12-11 15:32:59 +0800

    页面初始化及banner图片显示

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+const First = () => import('../views/First.vue')
+
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'First',
+    component: First
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/First.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main">
+    <div class="left"></div>
+    <div class="right">
+      <div class="top">
+        <img src="img/title.jpg" alt="">
+      </div>
+      <div class="buttom"></div>
+    </div>
+  </div>
+</template>
