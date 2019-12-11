484a9b3cadaabf78431c7778bfe3b68df665e576
WGF <546405494@qq.com>
2019-12-11 16:23:24 +0800

    导航栏数据显示

---
+++ src/components/LeftNav.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="left-nav">
+    <div class="title">热门推荐</div>
+    <ul>
+      <li v-for="(item, index) in menu" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeftNav',
+  data() {
+    return {
+      menu: [
+        '笔记本电脑',
+        '手机',
+        '笔记本电脑',
+        '手机',
+        '笔记本电脑',
+        '手机',
+        '笔记本电脑',
+        '手机',
+        '笔记本电脑',
+        '手机',
+        '笔记本电脑',
+        '手机',
+        '笔记本电脑',
+        '手机'
+      ]
+    }
+  },
+}
+</script>
--- src/views/First.vue
+++ src/views/First.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="main">
-    <div class="left"></div>
+    <div class="left">
+      <left-nav></left-nav>
+    </div>
     <div class="right">
       <div class="top">
         <img src="img/title.jpg" alt="">
@@ -9,3 +11,47 @@
     </div>
   </div>
 </template>
+
+<script>
+import LeftNav from '../components/LeftNav'
+export default {
+  name: 'Main',
+  components: {
+    LeftNav
+  }
+}
+</script>
+
+<style type="text/css">
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  .main {
+    width:1400px;
+    margin: 20px auto;
+  }
+
+  .left {
+    width: 150px;
+    float: left;
+    margin-right: 10px;
+  }
+
+  .right {
+    width: 1200px;
+    float: left;
+    margin-left: 10px;
+  }
+
+  .main .right .top img {
+    height: 200px;
+    width: 1200px;
+  }
+
+  .left,.right {
+    background: blue;
+    height: 500px;
+  }
+</style>
